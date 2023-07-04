dd01ce8b200c21f88c3a254dc17ca1ae37d9914b
elias <kompanipejper@gmail.com>
2023-07-05 04:11:55 +0900

    add db

--- dj_db.sql
+++ dj_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 04, 2023 at 10:40 AM
+-- Generation Time: Jul 04, 2023 at 07:11 PM
 -- Server version: 8.0.21
 -- PHP Version: 7.3.21
 
@@ -77,7 +77,34 @@ CREATE TABLE IF NOT EXISTS `request` (
   `requester_id` int DEFAULT NULL,
   `date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `request`
+--
+
+INSERT INTO `request` (`id`, `song_id`, `singer`, `dj`, `requester_id`, `date`) VALUES
+(10, 4, 'elas', 'fasd', 12, '2023-07-04 17:49:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request_setting`
+--
+
+DROP TABLE IF EXISTS `request_setting`;
+CREATE TABLE IF NOT EXISTS `request_setting` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `turn_on` tinyint NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `request_setting`
+--
+
+INSERT INTO `request_setting` (`id`, `turn_on`) VALUES
+(1, 1);
 
 -- --------------------------------------------------------
 
@@ -91,7 +118,15 @@ CREATE TABLE IF NOT EXISTS `songs` (
   `title` varchar(255) DEFAULT NULL,
   `artist` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `songs`
+--
+
+INSERT INTO `songs` (`id`, `title`, `artist`) VALUES
+(4, 'asdf', 'dsaf'),
+(3, 'fasdf', 'sdaf');
 
 -- --------------------------------------------------------
 
@@ -104,25 +139,22 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
   `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `verified` tinyint NOT NULL DEFAULT '0',
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `verify_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `verify_number` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `role` tinyint NOT NULL DEFAULT '0',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `verified`, `password`, `phone`, `verify_number`, `remember_token`, `role`, `created_at`, `updated_at`) VALUES
-(9, 'speed', 'judy', 'speedjudy928@gmail.com', 1, '3dbe00a167653a1aaee01d93e77e730e', '+447893920069', '3990', '$2y$10$.psa/7m3VuICf8DAWQPMU.dEmXzCf7TKf4WSkIVYq3ULXuKkxC8f6', 1, NULL, NULL),
-(10, 'speed', 'ju', 'topdev928@gmail.com', 1, '3dbe00a167653a1aaee01d93e77e730e', '+447893920069', '7646', '$2y$10$Xu.4z.ATKXPjSTaStfsjsuj4DKewuMa8pt42ZoO0OC65rv3uuKYcK', 0, NULL, NULL);
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `verified`, `phone`, `verify_number`, `remember_token`, `role`, `created_at`, `updated_at`) VALUES
+(12, 'Elias', 'Sbehat', 1, '447893920072', '9884', '$2y$10$edf3kIzmWNdkLsKx3It1leOKxgNFqQ9U1yW9KOxb8ppHBQijsv786', 1, NULL, NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
